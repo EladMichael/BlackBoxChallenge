740cd09ca87e9fd07b7448b5ed0306e62be85c17
EladMichael <elad.michael@gmail.com>
2016-04-08 14:47:36 -0400

    Update of v1_bot
    Try running this for me? I have numpy build issues

--- v1_bot.py
+++ v1_bot.py
@@ -3,31 +3,30 @@ import theano
 import numpy as np
 import theano.tensor as T
 import lasagne
+import time
 
-def prepare_agent():
-    net = lasagne.layers.InputLayer(shape=(None,1,1,n_features))
-    net = lasagne.layers.DenseLayer(net,num_units=50,nonlinearity=lasagne.nonlinearity.tanh)
-    net = lasange.layers.DenseLayer(net,num_units=n_actions,nonlinearity=lasagne.nonlinearity.softmax)
+def prepare_agent(in_state=None):
+    net = lasagne.layers.InputLayer(shape=(1,n_features),input_var=in_state)
+    net = lasagne.layers.DenseLayer(net,num_units=50,nonlinearity=lasagne.nonlinearities.tanh)
+    net = lasagne.layers.DenseLayer(net,num_units=n_actions,nonlinearity=lasagne.nonlinearities.softmax)
     return net
 
-def get_all_scores(state,verbose=0):
+def get_all_scores(state=None,verbose=0):
     checkpoint_id = bbox.create_checkpoint()
-    all_scores = np.array(1,n_actions)
+    all_scores = np.zeros(shape=n_actions)
     for a in range(n_actions):
         bbox.do_action(a)
         all_scores[a]=bbox.get_score()
         bbox.load_from_checkpoint(checkpoint_id)
     return all_scores
 
-def get_action_by_state(state, verbose=0):
-    if verbose:
-        for i in range(n_features):
-            print ("state[%d] = %f" %  (i, state[i]))
-
-        print ("score = {}, time = {}".format(bbox.get_score(), bbox.get_time()))
-
-    action_to_do = 0
+def get_action_by_state(state=None):
+    scores = get_all_scores(state)
+    action_to_do = np.argmax(scores)
+    # print (scores,action_to_do)
+    # raw_input("Press Enter to continue...")
     return action_to_do
+
 
 n_features = n_actions = max_time = -1
 
@@ -46,16 +45,40 @@ def prepare_bbox():
  
 def run_bbox(verbose=False):
     has_next = 1
-    
-    prepare_bbox()
-    agent=prepare_agent()
 
+    prepare_bbox()
+    #vector of the current state features
+    input_var= T.dvector('in_state')
+    input_var= T.reshape(input_var,(1,n_features))
+    #vector of the scores for 100 of the same action
+    target_var = T.dvector('scores')
+    target_var = T.reshape(target_var,(1,n_actions))
+    #Load net into the agent object
+    agent=prepare_agent(input_var)
+    #what the agent thinks will happen if it does each action 100 times
+    attempt = lasagne.layers.get_output(agent)
+    #how much the agent was wrong, and should be punished
+    punish = lasagne.objectives.squared_error(attempt,target_var)
+    punish = punish.mean()
+    #get the parameters for updating
+    params = lasagne.layers.get_all_params(agent,trainable=True)
+    #update the net with the error
+    teach = lasagne.updates.nesterov_momentum(punish,params,learning_rate=.1,momentum=.9)
+    #function to do all of the stuff above
+    train_fn = theano.function([input_var, target_var], punish, updates=teach,on_unused_input='ignore')
+    #time to check how long it takes to run
+    start = time.time()
     while has_next:
         state = bbox.get_state()
-        print np.shape(state)
-        action = get_action_by_state(state)
+        r_state= np.reshape(state,(1,n_features))
+        scores = get_all_scores(state)
+        r_scores = np.reshape(scores,(1,n_actions))
+        action = T.argmax(scores)
+        error = train_fn(r_state,r_scores)
+        print (error)
         has_next = bbox.do_action(action)
  
+    print ("Time to run: {} seconds".format(time.time()-start))
     bbox.finish(verbose=1)
  
  
